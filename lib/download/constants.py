610d9d9c6f9ff7932feb49a9a2bd9c34fb8c264b
Nabeel Prior <nabeel@brainjar.co.za>
2025-11-17 20:00:42 +0200

    Update MAX_ITEMS in Config to 10000 for improved item handling

--- lib/download/constants.py
+++ lib/download/constants.py
@@ -4,6 +4,6 @@ from dataclasses import dataclass
 class Config:
     """Configuration settings for the application"""
     PAGE_SIZE: int = 100
-    MAX_ITEMS: int = 1000
+    MAX_ITEMS: int = 10000
     INCLUDE_PHOTOS: bool = False
     BASE_URL: str = "https://api.gopro.com"
